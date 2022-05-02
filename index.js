a7bf950094bc9c40b409f1ccc85959171f7fcb1f
Vinny D <vdurham@prideseals.com>
2022-05-02 19:02:03 -0400

    a fun little start

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const { allowedNodeEnvironmentFlags, exit } = require('process');
 
 const startTrack = () => {
     console.log("Welcome to Employee Tracker!");
+    console.log("|==============================================|");
+    console.log("|   ____                                       |");  
+    console.log("|  |       _   _   _      __        __  __     |");   
+    console.log("|  |---   | | | | |D)|   |  | |__| |_  |_      |");  
+    console.log("|  |____  |  V  | |  |__ |__|   |  |__ |__     |");  
+    console.log("|                                              |");  
+    console.log("|  _______                                     |");  
+    console.log("|     |     _    _    __        __   _         |");  
+    console.log("|     |    |D)  (_)  |    |_/  |_   |D)        |");  
+    console.log("|     |    | | |   | |__  | |  |__  | |        |");  
+    console.log("|==============================================|");
     firstQuestion();
 };
 
